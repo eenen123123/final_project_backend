a476e5e051eea8d7022a5e8724c8c3dd00f4a435
gujeompal <yhg3877@naver.com>
2026-05-21 10:50:45 +0900

    불필요한 임포트 정리

--- admin/src/main/java/kr/or/ddit/exception/AdminExceptionHandler.java
+++ admin/src/main/java/kr/or/ddit/exception/AdminExceptionHandler.java
@@ -1,13 +1,14 @@
 package kr.or.ddit.exception;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import kr.or.ddit.finalProject.exception.FinalProjectException;
 
 @ControllerAdvice
 public class AdminExceptionHandler {
+
     @ExceptionHandler(FinalProjectException.class)
     public String handle(FinalProjectException ex, RedirectAttributes model) {
         model.addFlashAttribute("status", ex.getErrorCode().getMessage());
