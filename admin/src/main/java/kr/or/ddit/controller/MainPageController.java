63fa450a8d47c6c7ff17e0c771b594d51aa2f615
gujeompal <yhg3877@naver.com>
2026-05-21 11:31:22 +0900

    html 페이지 타이틀 정하는 법 변경 : html 파일에 <title th:fragment="pageTitle"> 태그 한줄 넣으면 됨

--- admin/src/main/java/kr/or/ddit/controller/MainPageController.java
+++ admin/src/main/java/kr/or/ddit/controller/MainPageController.java
@@ -1,15 +1,13 @@
 package kr.or.ddit.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
 
     @GetMapping("/")
-    public String mainPage(Model model) {
-        model.addAttribute("pageTitle", "Main Page");
+    public String mainPage() {
         return "admin:/main";
     }
 }
--- admin/src/main/java/kr/or/ddit/controller/example/ExampleHtmlController.java
+++ admin/src/main/java/kr/or/ddit/controller/example/ExampleHtmlController.java
@@ -1,15 +1,14 @@
 package kr.or.ddit.controller.example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExampleHtmlController {
 
     @GetMapping("/vod")
-    public String example1(Model model) {
-        model.addAttribute("pageTitle", "example1");
+    public String example1() {
+
         return "admin:/vod_management";
     }
 
