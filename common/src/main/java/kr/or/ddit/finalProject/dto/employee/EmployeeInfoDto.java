ffb3a2857bc32636b77f0ff72bfda214fef6da73
HoCheon95 <dcr584@gmail.com>
2026-05-27 15:11:46 +0900

    직원 등록 기능 추가: 신규 직원 등록 처리 로직 구현 및 관련 DTO, Mapper, Service 추가

--- common/src/main/java/kr/or/ddit/finalProject/dto/employee/EmployeeInfoDto.java
+++ common/src/main/java/kr/or/ddit/finalProject/dto/employee/EmployeeInfoDto.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +20,10 @@ public class EmployeeInfoDto implements Serializable {
     private String userId; // 기본키(PK)
     private String deptCd; // 부서 테이블 참조(FK)
     private String jbgrCd; // 직급 테이블 참조(FK)
-    private LocalDateTime joinYmd; // 실제 출근 시작 날짜
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate joinYmd; // 실제 출근 시작 날짜 (시/분/초 제외)
+    
     private LocalDateTime retmtYmd;
     private String emplStatCd; // 재직,휴직,퇴사 등 상태값 (01:재직 02:휴직 03:퇴사)
     private String atchFileId; // 공통첨부파일분류
