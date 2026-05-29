7496b32d7edffdafb3a7f8bddb62af8f79ed97c1
gujeompal <yhg3877@naver.com>
2026-05-29 16:38:46 +0900

    클래스룸 컨트롤러 구현중

---
+++ admin/src/main/java/kr/or/ddit/controller/instructor/InstructorClassroomController.java
@@ -0,0 +1,30 @@
+package kr.or.ddit.controller.instructor;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.ddit.finalProject.dto.classroom.ClassroomListResponse;
+import kr.or.ddit.finalProject.service.classroom.ClassroomService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/instructor/classroom")
+@RequiredArgsConstructor
+public class InstructorClassroomController {
+
+    private final ClassroomService classroomService;
+
+    @GetMapping("/list")
+    public String classroomList(Model model, Authentication authentication) {
+        String instrUserId = authentication.getName();
+        List<ClassroomListResponse> classroomList = classroomService.retrieveClassroomList(instrUserId);
+        model.addAttribute("classroomList", classroomList);
+        return "admin:/instructor/classroomList";
+    }
+
+}
