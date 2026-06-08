d2156088f6df765d1c3d89ac2a2d6bd5c9f21fd6
gujeompal <yhg3877@naver.com>
2026-06-08 11:16:18 +0900

    feat: 학생 클래스룸 목록 및 상세 조회 REST API 추가

--- common/src/main/java/kr/or/ddit/finalProject/mapper/classroom/ClassroomMemberMapper.java
+++ common/src/main/java/kr/or/ddit/finalProject/mapper/classroom/ClassroomMemberMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import kr.or.ddit.finalProject.dto.classroom.ClassroomGradeDto;
+import kr.or.ddit.finalProject.dto.classroom.ClassroomListResponse;
 import kr.or.ddit.finalProject.dto.classroom.ClassroomMemberListResponse;
 
 @Mapper
@@ -14,5 +15,7 @@ public interface ClassroomMemberMapper {
     List<ClassroomMemberListResponse> selectMembersByClassSn(@Param("classSn") Long classSn);
 
     List<ClassroomGradeDto> selectGradeList(@Param("classSn") Long classSn);
+
+    List<ClassroomListResponse> selectClassroomsByUserId(@Param("userId") String userId);
 
 }
