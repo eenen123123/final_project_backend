9c756629c7ffe00c319aab59e464ab2d67296276
HoCheon95 <dcr584@gmail.com>
2026-06-10 15:47:29 +0900

    회원 활동 로그 기록 기능 추가: DTO, 매퍼 및 AOP 구현

---
+++ common/src/main/java/kr/or/ddit/finalProject/dto/log/MemberActivityLogDto.java
@@ -0,0 +1,19 @@
+package kr.or.ddit.finalProject.dto.log;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberActivityLogDto {
+    private Long activityId;
+    private String userId;
+    private String activityType;
+    private String targetId;
+    private String activityIp;
+    private String createdAt;
+}
