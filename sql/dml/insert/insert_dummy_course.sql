96894f96a384ab460115b687a7699599aba90039
gujeompal <yhg3877@naver.com>
2026-06-10 09:44:19 +0900

    강좌 더미 데이터 새로 삽입

--- sql/dml/insert/insert_dummy_course.sql
+++ sql/dml/insert/insert_dummy_course.sql
@@ -55,3 +55,39 @@ select uc.constraint_name,
 on uc.constraint_name = ucc.constraint_name
  where uc.table_name = 'COURSE'
    and uc.constraint_type = 'R';
+
+
+   -- COURSE 테스트 데이터 삽입
+-- INSTR_USER_ID, CURRICULUM_ID는 실제 존재하는 값으로 교체 필요
+insert into course (
+   instr_user_id,
+   course_nm,
+   course_expln_cn,
+   tot_lrn_time_cnt,
+   opnn_yn,
+   course_price,
+   sort_ord,
+   curriculum_id,
+   rgtr_id,
+   reg_dt
+) values ( 'testuser05',        -- 실제 강사 USER_ID로 교체
+           '지수함수와 로그함수',
+           '▣ 평가원과 기출을 분석하여 필수 개념만을 녹여낸 수능에 가장 최적화된 강의
+
+▣ 수능수학 교과서개념+실전개념까지 수능에 꼭 필요한 수능개념 완성
+
+▣ 개념에 이은 수능유형분석을 통해 실전 적응력까지 배양
+
+▣ 확실한 개념 완성을 통해 어떤 유형의 문제가 나와도 흔들리지 않는 응용력과 실전능력을 향상시킨다.
+
+▣ 강의 수강 후, 혼자서 때려잡기를 통해 스스로 생각하여 문제를 해결하는 능력을 키울 수 있다.
+
+▣ 5단계 복습을 통해 부족하거나 어려운 개념은 더 철저히 이해할 수 있다.',-- 썸네일 (없으면 NULL)
+           '10:30:00',            -- HH:MM:SS 형식
+           'Y',                   -- 공개 여부
+           250000,
+           0,                     -- 커리큘럼 내 노출 순서
+           61,                     -- 실제 CURRICULUM_ID로 교체
+           'testuser05',        -- 등록자 ID
+           current_timestamp );
+commit;
