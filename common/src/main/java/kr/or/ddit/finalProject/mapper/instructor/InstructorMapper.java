6556b0757145e04320375a87cc6a53e4f08b9962
gujeompal <yhg3877@naver.com>
2026-06-11 11:04:45 +0900

    프론트용 강사 목록 조회 기능 추가 및 기존 기능 개선

--- common/src/main/java/kr/or/ddit/finalProject/mapper/instructor/InstructorMapper.java
+++ common/src/main/java/kr/or/ddit/finalProject/mapper/instructor/InstructorMapper.java
@@ -11,7 +11,13 @@ import kr.or.ddit.finalProject.dto.instructor.InstructorListResponse;
 @Mapper
 public interface InstructorMapper {
 
-    List<InstructorListResponse> selectInstructorsBySubject();
+    /**
+     * 강사 조회
+     *
+     * @param subjClId
+     * @return
+     */
+    List<InstructorListResponse> selectInstructors(@Param("subjClId") Long subjClId);
 
     InstructorDetailResponse selectInstructorByUuid(@Param("instrUuid") String instrUuid);
 
